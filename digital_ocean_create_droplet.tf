0d42cb528acd684ce3fb9960642fbc9e60b7d705
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-09-20 11:03:08 +0300

    Clean up configs

--- digital_ocean_create_droplet.tf
+++ digital_ocean_create_droplet.tf
@@ -1,10 +1,10 @@
 # Define variables to be used
-variable "digitalocean_token" {} // Comes from secrets.tfvars
-variable "domain_name" {} // Comes from secrets.tfvars
-variable "droplet_name" {} // Comes from terraform.tfvars
-variable "droplet_region" {} // Comes from terraform.tfvars
-variable "droplet_size" {} // Comes from terraform.tfvars
-variable "droplet_backups" {} // Comes from terraform.tfvars
+variable "digitalocean_token" {}
+variable "domain_name" {}
+variable "droplet_name" {}
+variable "droplet_region" {}
+variable "droplet_size" {}
+variable "droplet_backups" {}
 variable "create_domain" {
   description = "If set to true, create a domain and a CNAME record for this resource"
 }
@@ -20,52 +20,52 @@ provider "digitalocean" {
 # The web word is arbitrary, it serves the purpose of an identifier in this
 # declaration, as such, you can choose whatever name you’re most comfortable with.
 resource "digitalocean_droplet" "web" {
-  image  = "ubuntu-16-04-x64"
-  name   = "${var.droplet_name}"
-  region = "${var.droplet_region}"
-  size   = "${var.droplet_size}"
-  backups = "${var.droplet_backups}"
+    image   = "ubuntu-16-04-x64"
+    name    = "${var.droplet_name}"
+    region  = "${var.droplet_region}"
+    size    = "${var.droplet_size}"
+    backups = "${var.droplet_backups}"
 }
 
 
 # Create a new domain
 # NOTE: This & the record are not created if `create_domain` is `false`
 resource "digitalocean_domain" "web" {
-    count = "${var.create_domain}"
-    name = "${var.domain_name}"
+    count      = "${var.create_domain}"
+    name       = "${var.domain_name}"
     ip_address = "${digitalocean_droplet.web.ipv4_address}"
 }
 
 # Add a CNAME record to the domain
 # NOTE: This & the domain are not created if `create_domain` is `false`
 resource "digitalocean_record" "web" {
-    count = "${var.create_domain}"
+    count  = "${var.create_domain}"
     domain = "${digitalocean_domain.web.name}"
-    type = "CNAME"
-    value =  "${digitalocean_domain.web.name}."
-    name = "www"
+    type   = "CNAME"
+    value  =  "${digitalocean_domain.web.name}."
+    name   = "www"
 }
 
 
 # Define output variables to be displayed after creation
 output "Domain" {
-  value = "${digitalocean_domain.web.id}"
+    value = "${digitalocean_domain.web.id}"
 }
 output "Droplet_name" {
-  value = "${digitalocean_droplet.web.name}"
+    value = "${digitalocean_droplet.web.name}"
 }
 output "IP" {
-  value = "${digitalocean_droplet.web.ipv4_address}"
+    value = "${digitalocean_droplet.web.ipv4_address}"
 }
 output "Region" {
-  value = "${digitalocean_droplet.web.region}"
+    value = "${digitalocean_droplet.web.region}"
 }
 output "Status" {
-  value = "${digitalocean_droplet.web.status}"
+    value = "${digitalocean_droplet.web.status}"
 }
 output "Size" {
-  value = "${digitalocean_droplet.web.size}"
+    value = "${digitalocean_droplet.web.size}"
 }
 output "Price_monthly" {
-  value = "${digitalocean_droplet.web.price_monthly}"
+    value = "${digitalocean_droplet.web.price_monthly}"
 }
