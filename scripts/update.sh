072dcae794c918a045d9a7a5e498ef0e82662597
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-09-22 17:58:28 +0300

    Dont install unattended updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Run apt-get update, upgrade and install unattended-upgrades
+# Run apt-get update, upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y update &&
-DEBIAN_FRONTEND=noninteractive apt-get -y upgrade &&
-DEBIAN_FRONTEND=noninteractive apt-get -y install unattended-upgrades
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
